3c61cf7ae69f422fa559161c33edd8af443e7d63
frozyum <smamu13@freeuni.edu.ge>
2020-09-28 19:57:28 +0400

    Remove authentication on ping view

--- ping/views.py
+++ ping/views.py
@@ -1,13 +1,7 @@
 from django.http import HttpResponse
 from datetime import datetime
 
-from rest_framework.permissions import IsAuthenticated, AllowAny
-from rest_framework.decorators import permission_classes, authentication_classes
 
-from authentication.decorators import base_auth_required
-
-
-@base_auth_required
 def ping_handler(request):
     now = datetime.now()
     dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
