6a1429b581636798b4199f4d73d4ec35aeb34a3b
frozyum <smamu13@freeuni.edu.ge>
2020-09-28 20:44:45 +0400

    Remove authentication from ping endpoint

--- ping/views.py
+++ ping/views.py
@@ -1,11 +1,9 @@
 from django.http import HttpResponse
 from datetime import datetime
 
-from authentication.decorators import base_auth_required
 from utils.datetransformations import default_date_format
 
 
-@base_auth_required
 def ping_handler(request):
     now = datetime.now()
     dt_string = now.strftime(default_date_format)
