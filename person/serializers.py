4be2e6562544d4f4fbe532618b8a4e691eed7188
frozyum <smamu13@freeuni.edu.ge>
2020-10-03 11:12:53 +0400

    change person fields, add ordering.

--- person/serializers.py
+++ person/serializers.py
@@ -1,9 +1,13 @@
 from rest_framework.serializers import ModelSerializer
-
+from expense.serializers import ExpenseSerializer
 from .models import Person
 
 
 class PersonSerializer(ModelSerializer):
+    persons = ExpenseSerializer(many=True, read_only=True)
+
     class Meta:
+        ordering = ['-id']
         model = Person
-        fields = ['name']
+        fields = ['id', 'name', 'persons']
+        extra_kwargs = {'persons': {'required': False}}
