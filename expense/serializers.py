892cc16a44ea5a771899307a615743ba3b0d4485
frozyum <smamu13@freeuni.edu.ge>
2020-10-03 10:54:03 +0400

    make serializer file in expense app

---
+++ expense/serializers.py
@@ -0,0 +1,10 @@
+from rest_framework.serializers import ModelSerializer
+from .models import Expense
+
+
+class ExpenseSerializer(ModelSerializer):
+    class Meta:
+        ordering = ['-id']
+        model = Expense
+        fields = ['id', 'title', 'amount', 'date', 'currency', 'by', 'too']
+        extra_kwargs = {'books': {'required': False}}
