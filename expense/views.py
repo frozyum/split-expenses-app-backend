7f81afbd3bac0501ef565af95b6bffbb9e052223
frozyum <smamu13@freeuni.edu.ge>
2020-10-27 13:01:36 +0400

    empty extra spaces

--- expense/views.py
+++ expense/views.py
@@ -1,21 +1,10 @@
 import json
-
 from django.http import HttpResponse
-from django.shortcuts import render
 from rest_framework.decorators import api_view
-from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
-
-from authentication.models import User
-from group.models import Group
-from .serializers import ExpenseSerializer
-from rest_framework import permissions
-from .models import Expense
-from person.models import Person
-from payment.models import Payment
-
-# Create your views here.
 from .services.reportservice import get_transactions_report, get_persons_report
 
+
+# Create your views here.
 
 @api_view(['GET'])
 def get_report(request, group_id):
